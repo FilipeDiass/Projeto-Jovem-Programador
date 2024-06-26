68c8060be7b6f52dd6ec1bf40f2f12daf23691ef
Jamille <jamille.araujo.freitas2003@gmail.com>
2024-06-26 18:01:29 -0300

    criação do component videos

---
+++ scripts/videos/dataVideos.js
@@ -0,0 +1,82 @@
+export const dataVideos = [
+  [
+    {
+        image: "../../images/thumbs-videos/video01-thumb.png",
+        channelProfile: "../../images/thumb-profile/channel01-profile.png",
+        title: "Breaking! NASA discovers alien life on Mars ",
+        channel: "NASA",
+        views: "99.2M views",
+        publication: "Now"
+    },
+  ],
+  [
+    {
+        image: "../../images/thumbs-videos/video02-thumb.png",
+        channelProfile: "../../images/thumb-profile/channel02-profile.png",
+        title: "Volcano goes boom",
+        channel: "BoomChannel",
+        views: "82K views",
+        publication: "6 months ago"
+    }
+  ],
+  [
+    {
+        image: "../../images/thumbs-videos/video03-thumb.png",
+        channelProfile: "../../images/thumb-profile/channel03-profile.png",
+        title: "DesignER-rrors",
+        channel: "UxDesign",
+        views: "120K views",
+        publication: "2 months ago"
+    }
+  ],
+  [
+    {
+        image: "../../images/thumbs-videos/video04-thumb.png",
+        channelProfile: "../../images/thumb-profile/channel04-profile.png",
+        title: "1 year traveling with my van in Europe!",
+        channel: "Bas",
+        views: "1K views",
+        publication: "11 months ago"
+    },
+  ],
+  [
+    {
+        image: "../../images/thumbs-videos/video05-thumb.png",
+        channelProfile: "../../images/thumb-profile/channel05-profile.png",
+        title: "Random bubble designstuff",
+        channel: "UIlover",
+        views: "82K views",
+        publication: "6 months ago"
+    }
+  ],
+  [
+    {
+        image: "../../images/thumbs-videos/video06-thumb.png",
+        channelProfile: "../../images/thumb-profile/channel06-profile.png",
+        title: "The Designosaur",
+        channel: "MrDesignosaur",
+        views: "82K views",
+        publication: "6 months ago"
+    },
+  ],
+  [
+    {
+        image: "../../images/thumbs-videos/video07-thumb.png",
+        channelProfile: "../../images/thumb-profile/channel07-profile.png",
+        title: "Autolayout for the win!",
+        channel: "Figma",
+        views: "2.3M views",
+        publication: "9 months ago"
+    },
+  ],
+  [
+    {
+        image: "../../images/thumbs-videos/video08-thumb.png",
+        channelProfile: "../../images/thumb-profile/channel08-profile.png",
+        title: "Kitten fighting in bar over spilled milk caught on tape",
+        channel: "CatsWithHeads",
+        views: "9M views",
+        publication: "1 months ago"
+    },
+  ]
+];

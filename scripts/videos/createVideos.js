f7d15bbd9f170adf6525891890de5938f3b28239
Filipe <diasjoaum@gmail.com>
2024-06-26 20:23:46 -0300

    melhorando layouts

--- scripts/videos/createVideos.js
+++ scripts/videos/createVideos.js
@@ -1,33 +1,36 @@
 import { dataVideos } from "./dataVideos.js";
 
 function createVideos(i) {
-    // Seleciona o conjunto de dados baseado no índice i
-    const data = dataVideos[i];
+  // Seleciona o conjunto de dados baseado no índice i
+  const data = dataVideos[i];
 
-    return `
+  return `
         <div class="videos-container">
-            ${data.map(card => `
+            ${data
+              .map(
+                (card) => `
                 <div class="video-card">
                     <img src="${card.image}" alt="video thumb" class="video-thumb">
                     <div class="card-details">
+                      <div class="container-details">
                         <img src="${card.channelProfile}" alt="channel profile" class="channel-profile">
-                        
-						<div class="video-info">
-                            <h2 class="title">${card.title}</h2>
-							
-							<div>
-								<p class="channel">${card.channel}</p>
-								<p> . </p>
-								<p class="views">${card.views}</p>
-								<p> . </p>
-								<p class="publication">${card.publication}</p> 
-							</div>                      
-						</div>
-						
-						<span class="material-symbols-outlined">more_vert</span>
+                        <div class="video-info">
+                          <h2 class="title">${card.title}</h2>
+                          <div>
+                            <p class="channel">${card.channel}</p>
+                            <p> . </p>
+                            <p class="views">${card.views}</p>
+                            <p> . </p>
+                            <p class="publication">${card.publication}</p> 
+                          </div>                      
+                        </div>
+                      </div>  
+						          <span class="material-symbols-outlined">more_vert</span>
                     </div>
-                </div>
-            `).join('')}
+            </div>
+            `
+              )
+              .join("")}
         </div>`;
 }
 
